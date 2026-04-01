35f5b9fa80b38a39d7005ac3189f7d74fcbecbd9
Aman Bhardwaj <32950098+Strider912@users.noreply.github.com>
2026-04-01 21:19:36 +0530

    Solved Missing number
    Solved missing number

---
+++ DSA/Array/missing-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+
+    let partialSum = 0
+    let n   = nums.length
+    let totalSum = n * (n+1)/2
+
+    for(let i=0; i<nums.length; i++){
+        partialSum = partialSum + nums[i]
+    }
+
+    return totalSum - partialSum
+};
+
+const result = missingNumber([3,0,1])
+const result2 = missingNumber([0,1])
+const result3 = missingNumber([9,6,4,2,3,5,7,0,1])
+
+console.log({result, result2, result3 });
