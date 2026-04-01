77abbe81aeeacb1a488ae2a80cf0763302780f2d
Aman Bhardwaj <32950098+Strider912@users.noreply.github.com>
2026-04-01 21:37:11 +0530

    Added brute force approach
    Added brute force approach to missing number problem

--- DSA/Array/missing-number.js
+++ DSA/Array/missing-number.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var missingNumber = function(nums) {
 
     let partialSum = 0
@@ -15,8 +16,26 @@ var missingNumber = function(nums) {
     return totalSum - partialSum
 };
 
-const result = missingNumber([3,0,1])
-const result2 = missingNumber([0,1])
-const result3 = missingNumber([9,6,4,2,3,5,7,0,1])
+const result = missingNumber([3, 0, 1]);
+const result2 = missingNumber([0, 1]);
+const result3 = missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]);
 
-console.log({result, result2, result3 });
+console.log({ result, result2, result3 });
+
+
+/*  Brute Force Approach:
+
+var missingNumber = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  let num = 0
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1] + 1) {
+      return nums[i - 1] + 1;
+    } else {
+      num = nums[i] + 1;
+    }
+  }
+  return num
+};
+*/
